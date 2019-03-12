f689e40052672c48c1f41ee4982aa5cd473fb1e1
daniel.plotkin <daniel.pltk@gmail.com>
2019-03-12 14:27:26 +0200

    my-home v1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,92 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AppService } from './app.service';
+
+// export interface IResponse<Object> {
+//   houses: Array<any>;
+// }
+
+export interface House<Object> {
+  'coords': {
+    'lat': number,
+    'lon': number
+  };
+  'params': {
+    'rooms': number,
+    'value': number
+  };
+  'street': string;
+  'distance': number;
+}
+export interface Houses<Array> {
+  houses: House[];
+}
+export interface BasePoint {
+  lon: number;
+  lat: number;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'my-home';
-  data;
-  constructor(private getHousesService: AppService ) {
-    this.data = this.getHousesService.housesResponseData;
+export class AppComponent implements OnInit {
+  title = 'My Home';
+  //houses: Array<any> = [];
+  //dataObj: IResponse<Object>;
+  houses: Houses[];
+  list1: Array<number> = [];
+  list2: Array<string> = [];
+  list3: Array<number> = [];
+  templist: any;
+  // basePoint: BasePoint;
+  basePoint: BasePoint = {
+    lon: 0,
+    lat: 0
+  };
+
+  constructor(private service: AppService) {
+  }
+
+  ngOnInit() {
+    this.service.getAllHousesFromApi().subscribe(
+      data => {
+        this.houses = Object.assign(data.houses); // Storing all the data in order to optimize performance and avoid unnecessary web calls
+        for (let i = 0; i < this.houses.length; i++) {
+          if (this.houses[i].params !== undefined && this.houses[i].params.rooms > 5) {
+            this.list1.push(this.houses[i].params.rooms);
+            this.list1.sort((a, b) => a - b);
+          }
+          // Since the Street names are already sorted alphabetically
+          this.list2.push(this.houses[i].street);
+          this.list2.sort(); // This step is unnecessary in case the results already sorted.
+          // Handle basePoint for Distance measure
+          if (this.houses[i].street !== undefined && this.houses[i].street === 'Eberswalder Straße 55') {
+            this.basePoint.lat = this.houses[i].coords.lat;
+            this.basePoint.lon = this.houses[i].coords.lon;
+            this.templist = this.basePoint;
+          }
+          // this.list3.push(this.houses[i]);
+        }
+      }
+    );
+  }
+
+  getDistanceByCord(lat1, lon1, lat2, lon2) {
+    const R = 6371; // Constant as earth radius in km
+    let disLat = this.deg2rad(lat2 - lat1);
+    let disLon = this.deg2rad(lon2 - lon1);
+    let a =
+      Math.sin(disLat / 2) * Math.sin(disLat / 2) +
+      Math.cos(this.deg2rad(lat1)) * Math.cos(this.deg2rad(lat2)) *
+      Math.sin(disLon / 2) * Math.sin(disLon / 2)
+    ;
+    let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    let d = R * c; // Distance in km
+    return d;
+  }
+
+  deg2rad(deg) {
+    return deg * (Math.PI / 180);
   }
 }
--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,28 +1,13 @@
+
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-export interface IResponse<object> {
-  houses: Array<any>;
-}
-
 @Injectable()
 export class AppService {
-  housesResponseData: Array<any> = [ ];
-  dataObj: object;
-  constructor(private httpService: HttpClient) {
-    this.getAllHousesFromApi().subscribe(
-      data => {
+  constructor(private http: HttpClient) {}
 
-        // this.dataObj = JSON.parse(data);
-        this.dataObj = data;
-        this.housesResponseData.push(this.dataObj.houses);
-        console.log(this.housesResponseData);
-      }
-    );
-  }
-
-  getAllHousesFromApi(): Observable<IResponse> {
-    return this.httpService.get<IResponse>('./assets/houses.json');
+  getAllHousesFromApi(): Observable<any> {
+    return this.http.get('./assets/houses.json');
   }
 }
