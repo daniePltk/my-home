8345165f30ddd878c479fca76543785736c32c1b
daniel.plotkin <daniel.pltk@gmail.com>
2019-03-14 14:48:23 +0200

    my-home preFinal v with Errors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterContentInit, Component, OnInit } from '@angular/core';
 import { AppService } from './app.service';
 import { MapsAPILoader } from '@agm/core';
+import { finalize } from 'rxjs/operators';
 declare var google;
 
 
@@ -34,21 +35,23 @@ export interface BasePoint {
 export class AppComponent implements OnInit {
   title = 'My Home';
   houses: Houses;
-  list1: Array<House> = [];
-  list2: Array<House> = [];
-  list3: Array<House> = [];
+  housesSortedByRoomsNum: Array<House> = [];
+  housesWithoutFullInfo: Array<House> = [];
+  housesSortedByClosenessToSister: Array<House> = [];
   basePoint: BasePoint = {
     lon: 0,
     lat: 0
   };
   zoom = 11;
   markers = [];
-  filteredMarkers = [];
   housescords = [];
   housesAllInfo = [];
   distanceInKm;
-  in_criteria_houses = []
-  constructor(private service: AppService, private mapsAPILoader: MapsAPILoader) {
+  inCriteriaHouses: Array<House> = [];
+  myFinalHome: House;
+
+  constructor(private service: AppService,
+              private mapsAPILoader: MapsAPILoader) {
   }
 
   ngOnInit() {
@@ -66,43 +69,43 @@ export class AppComponent implements OnInit {
               const markerLoc = new google.maps.LatLng(this.houses[i].coords.lat, this.houses[i].coords.lon);
               const distance = google.maps.geometry.spherical.computeDistanceBetween(markerLoc, sistersHouse);
               this.houses[i].distanceFromSister = distance;
+            }).finally(() => {
+              // Sorting the houses with the optional property 'distanceFromSister'
+              this.housesAllInfo.sort((a, b) => a.distanceFromSister - b.distanceFromSister);
             });
             this.housesAllInfo.push(this.houses[i]);
           }
           if (this.houses[i].params !== undefined && this.houses[i].params.rooms > 5) {
-            this.list1.push(this.houses[i]);
-            this.list1.sort((a, b) => a.params.rooms - b.params.rooms);
+            this.housesSortedByRoomsNum.push(this.houses[i]);
+            this.housesSortedByRoomsNum.sort((a, b) => a.params.rooms - b.params.rooms);
           }
           if (!(this.houses[i].hasOwnProperty('params') && this.houses[i].params.hasOwnProperty('rooms') && this.houses[i].params.hasOwnProperty('value'))) {
-            this.list2.push(this.houses[i]);
-            this.list2.sort(this.streetSortByName('street'));
+            this.housesWithoutFullInfo.push(this.houses[i]);
+            this.housesWithoutFullInfo.sort(this.streetSortByName('street'));
           }
-          // console.log( this.houses[i].params !== undefined);
           if (this.houses[i].params !== undefined) {
             if (this.houses[i].params.value !== undefined &&
               this.houses[i].params.rooms !== undefined && this.houses[i].street !== 'Eberswalder Straße 55' &&
               this.houses[i].params.rooms >= 10 && this.houses[i].params.value <= 5000000) {
-              this.in_criteria_houses = this.houses[i];
-              console.log(this.houses[i]);
+              this.inCriteriaHouses.push(this.houses[i]);
             }
           }
         }
+        // Since the list will be sorted according closest distance and filtered by other criteria we pick the first instance
+        this.myFinalHome = this.inCriteriaHouses[0];
+
         this.markers = this.getLocations();
         this.mapsAPILoader.load().then(() => {
           const sistersHouse = new google.maps.LatLng(this.basePoint.lat, this.basePoint.lon);
           this.housescords = this.markers.filter(m => {
             const markerLoc = new google.maps.LatLng(m.lat, m.lon);
             this.distanceInKm = google.maps.geometry.spherical.computeDistanceBetween(markerLoc, sistersHouse);
-            this.list3.push(this.distanceInKm);
-            this.list3.sort();
+            this.housesSortedByClosenessToSister.push(this.distanceInKm);
+            this.housesSortedByClosenessToSister.sort();
             return m;
           });
         });
-        this.housesAllInfo.sort((a, b) => a.distanceFromSister - b.distanceFromSister);
-        console.log('house records', this.housesAllInfo);
       });
-    // this.housesAllInfo.sort((a, b) => a.distanceFromSister - b.distanceFromSister);
-    // console.log('house records', this.housesAllInfo);
   }
   getLocations(): Array<{ latitude: number, longitude: number }> {
     return this.housescords;
